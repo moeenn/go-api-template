ec9cc0772e9d41147746eab197c503a57bf2056c
Muhammad Moeen <moeen.v8@gmail.com>
2024-03-04 19:01:53 +0500

    added linters to projects

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,6 +3,11 @@
 echo "Installing project dependencies"
 go mod tidy
 
+# linting dependencies
+go install honnef.co/go/tools/cmd/staticcheck@latest
+go install github.com/kisielk/errcheck@latest
+go install github.com/jgautheron/goconst/cmd/goconst@latest
+
 # list out installation commands for binary dependencies
 go install -v github.com/joho/godotenv/cmd/godotenv@latest
 go install -v github.com/moeenn/go-token@latest
